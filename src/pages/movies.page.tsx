409918d69e2986bd36f754ba1746abb61c289291
DavePaurillo <paurillo.dave@gmail.com>
2026-05-19 18:26:38 +0800

    feat: sorting - this is only per page because there is a limitation with the tmdb api

--- src/pages/movies.page.tsx
+++ src/pages/movies.page.tsx
@@ -1,13 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
+import { SortAscIcon, SortDescIcon } from 'lucide-react';
+import { useMemo, useState } from 'react';
 
 import { searchQueryOptions, trendingQueryOptions } from '@/api/movies.queries';
 import MovieList from '@/components/movie-list';
 import Pagination from '@/components/pagination';
+import { Button } from '@/components/ui/button';
 import { useMovieStore } from '@/store/movie.store';
 
 export default function MoviesPage() {
   const searchQuery = useMovieStore((s) => s.searchQuery);
   const page = useMovieStore((s) => s.page);
+  const [sortBy, setSortBy] = useState('none');
   const isSearching = searchQuery.length > 0;
 
   const trendingQuery = useQuery(trendingQueryOptions(page));
@@ -15,6 +19,17 @@ export default function MoviesPage() {
 
   const activeQuery = isSearching ? searchQueryResult : trendingQuery;
   const { data, isLoading, isError } = activeQuery;
+
+  const sortedData = useMemo(() => {
+    if (!data?.results) return data;
+    if (sortBy === 'none') return data;
+    return {
+      ...data,
+      results: [...data.results].sort((a, b) =>
+        sortBy === 'asc' ? a.vote_average - b.vote_average : b.vote_average - a.vote_average,
+      ),
+    };
+  }, [data, sortBy]);
 
   if (isLoading)
     return <p>{isSearching ? `Searching for "${searchQuery}"` : 'Loading trending movies.'}</p>;
@@ -37,7 +52,24 @@ export default function MoviesPage() {
 
   return (
     <>
-      <MovieList movies={data} />
+      <div className="flex items-center justify-end gap-2">
+        <p className="text-muted-foreground text-sm">Sort by Rating</p>
+        <Button
+          variant="outline"
+          onClick={() => {
+            setSortBy((prev) => {
+              if (prev === 'none') return 'asc';
+              if (prev === 'asc') return 'desc';
+              return 'none';
+            });
+          }}
+        >
+          {sortBy === 'none' && <span>—</span>}
+          {sortBy === 'asc' && <SortAscIcon />}
+          {sortBy === 'desc' && <SortDescIcon />}
+        </Button>
+      </div>
+      <MovieList movies={sortedData} />
       <Pagination page={data?.page} totalPages={data?.total_pages} />
     </>
   );
